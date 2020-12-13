d537483db69fcf11e67f37a8047aff59b1723789
FabioRosado <fabioglrosado@gmail.com>
2020-12-13 12:46:11 +0000

    Allow user to click progress bar and skip in video. Fix loop counter bug

--- src/components/video.js
+++ src/components/video.js
@@ -30,8 +30,12 @@ const VideoPlayer = () => {
     }
 
     const incrementCounter = () => {
-        if (getSeconds(videoRef.current.currentTime) > 9) {
-            setCount(counter + 1)
+        if (Math.round(videoRef.current.currentTime, 2) > 9) {
+            if (loop) {
+                setCount(counter + .8)
+            } else {
+                setCount(counter + .5)
+            }
         }        
     }
 
@@ -44,8 +48,22 @@ const VideoPlayer = () => {
         setPlayed(videoRef.current.currentTime)
         incrementCounter()
     }
+
+
+    const updateSeek = (event) => {
+        const progressBar = document.getElementById("progress-bar").offsetWidth
+        const player = document.getElementById("video-player")
+
+        const skipTo = Math.round((event.offsetX) * parseInt(progressBar, 10)/10000)
+
+        player.currentTime = skipTo
+
+    }
     
     useEffect(() => {
+
+        const seek = document.getElementById("progress-bar")
+
         const handleKeyPress = (event) => {
             if (event.key === "k") {
                 playOrPause()
@@ -64,6 +82,8 @@ const VideoPlayer = () => {
             player.addEventListener("timeupdate", getCurrentTime)
             player.addEventListener("playing", videoIsPlaying)
             player.addEventListener("pause", videoIsPaused)
+            seek.addEventListener("click", updateSeek)
+            
         }
 
         return () => {
@@ -80,28 +100,29 @@ const VideoPlayer = () => {
 
     return(
         <div className="relative">
+            {console.log(counter)}
             {played > 3.5 && played < 8.5 && counter < 1 && <img className="image-1" src="/images/image1.png" alt="banana" />}
             {played > 6 && played < 8 && counter < 3 && <img className="image-2" src="/images/image2.png" alt="remote" /> }
             {played > 7 && played < 8.5 && counter < 4 && <img className="image-3" src="/images/image3.png" alt="fire" />}
 
-            <video ref={videoRef} className="video-player" id="video-player"  controls loop={loop}>
+            <video ref={videoRef} className="video-player" id="video-player" loop={loop}>
                 <source src="/Big_Buck_Bunny_1080_10s_5MB.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
             <div className="video-controls">
                 <div className="left-side">
-                    <button className="player-button" aria-label="Play/Pause (shortcut k)" data-title="Play (k)" onClick={() => playOrPause()}>
+                    <button className="player-button" title="Play/Pause (k)" aria-label="Play/Pause (shortcut k)" data-title="Play (k)" onClick={() => playOrPause()}>
                         <Icon icon={isPlaying ? "pause" : "play"} width={20} height={23} />
                     </button>
 
-                    <button className="player-button disabled-button" aria-label="Volume Button" disabled>
+                    <button className="player-button disabled-button" title="Volume (disabled)" aria-label="Volume Button" disabled>
                         <Icon icon="volume" id="disabled-button" width={27} height={23} />
                     </button>
                 </div>
 
                 <div className="center-controls">
                     <div className="video-progress">
-                        <div className="progress-bar">
+                        <div id="progress-bar">
                             <div className="progress"  style={{width: `${played * 10}%`}} />
                         </div>
                     </div>
@@ -114,14 +135,11 @@ const VideoPlayer = () => {
 
                 </div>
 
-                
-
-
                 <div className="right-side">
-                    <button className="player-button" aria-label="Toggle video loopback (shorcut l)" onClick={() => toggleLoop()}>
+                    <button className="player-button" title="Loop (l)" aria-label="Toggle video loopback (shorcut l)" onClick={() => toggleLoop()}>
                         <Icon icon="loop" width={27} height={23} />
                     </button>
-                    <button className="player-button" aria-label="Toggle fullscreen (shorcut f)" onClick={() => toggleFullscreen(player)}>
+                    <button className="player-button" title="Fullscreen (f)" aria-label="Toggle fullscreen (shorcut f)" onClick={() => toggleFullscreen(player)}>
                         <Icon icon="expand" width={27} height={23} />
                     </button>
                 </div>
